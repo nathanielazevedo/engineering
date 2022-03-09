4361b38fe3e200bae33c809f27c204fb1a9039ab
Lohane G Diogo <lohane1.8gd@gmail.com>
2022-03-08 21:52:45 -0300

    #1 My name is missing an s

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Lori</h1>
+        <h1>Hello Loris</h1>
       </header>
     </div>
   );
