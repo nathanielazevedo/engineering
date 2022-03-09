ce927c1c466f79c50848cc457af1a4576617f62a
nathanielazevedo <nathanielazevedo@hotmail.com>
2022-03-08 23:57:00 -0500

    spacex

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./Nav/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Loris</h1>
-      </header>
+    <div className="App-header">
+      <Nav />
+      <div className="main-box">
+        <h1>FALCON 9</h1>
+        <h5 className="bottom">FIRST ORBITAL CLASS ROCKET CAPABLE OF REFLIGHT</h5>
+      </div>
     </div>
   );
 }
